fa94f35df7403b2fff6213fb442147eed3706b89
Maytham Fahmi <maythamfahmi@nextbix.dk>
2022-02-25 23:05:08 +0100

    #28 fix release tool

--- CreateRelease.ps1
+++ CreateRelease.ps1
@@ -7,10 +7,11 @@ param(
      [string]$Patch
  )
 
+
 $appVersion = (git describe --match "v*" main).replace("v", "").split(".")
 $majorDefault = $appVersion[0]
 $minorDefault = $appVersion[1]
-$patchDefault = $appVersion[2]
+$patchDefault = $appVersion[2].substring(0, $appVersion[2].Indexof("-"));
 
 Function Version{
     param(
@@ -18,7 +19,12 @@ Function Version{
         [Parameter(Mandatory=$true)] [ValidateRange(0,9)] [Int]$Minor,
         [Parameter(Mandatory=$true)] [ValidateRange(0,9)] [Int]$Patch
     )
-    $PatchInc = $Patch + 1;
+    [int]$PatchInc  = ($Patch + 3) -as [int];
+    if($PatchInc -gt 20){
+        $PatchInc = 0;
+        $Minor = $Minor + 1;
+    }
+
     $Version = "v$Major.$Minor.$PatchInc"
     Write-Output "$Version"
 
@@ -29,6 +35,7 @@ Function Version{
         {
             Write-output "Cleaning unused tags" 
             git fetch -p -P origin
+            Write-Output ${Version}
             Write-output "Create tag Release" 
             git tag ${Version} -a -m "Release ${Version}"
             Write-output "Push Release" 
@@ -44,3 +51,6 @@ $majorVal = If([string]::IsNullOrEmpty($Major)) {$majorDefault} else {$Major}
 $minorVal = If([string]::IsNullOrEmpty($Minor)) {$minorDefault} else {$Minor}
 $patchVal = If([string]::IsNullOrEmpty($Patch)) {$patchDefault} else {$Patch}
 Version -Major $majorVal -Minor $minorVal -Patch $patchVal
+
+# $v = [version]'1.1.1.0'
+# [version]::New($v.Major,$v.Minor,$v.Build+3,$v.Revision)
